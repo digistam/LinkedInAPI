b9f59040a50a786e01cc84fac30bf079e4cf6f89
digistam <digistam@gmail.com>
2014-11-10 11:12:16 +0100

    get token and credentials template files

---
+++ linkedincredentials.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# ? 2012, Mark Stam digistam@gmail.com 
+# Alle rechten voorbehouden.
+# Niets uit dit script mag worden verveelvoudigd, opgeslagen in een geautomatiseerd gegevensbestand en/of openbaar gemaakt in enige vorm of op enige wijze, hetzij elektronisch, mechanisch, door fotokopie?n, opnamen of op enige andere manier zonder voorafgaande schriftelijke toestemming van de auteur. 
+
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+USER_TOKEN = ''
+USER_SECRET = ''
